083a089933b5612bbd6f495679ab3f8d275106ee
JOSE MARIA GARCIA DAROCA <110504961+JOSEDAROCA@users.noreply.github.com>
2022-09-15 16:34:23 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+let btnEnv = document.getElementById("btnEnviar");
+let dato = document.getElementById("dato");
+let rotulo = document.getElementById("rotulo");
+let mensaje = document.getElementById("mensaje");
+
+rotulo.innerHTML = "Indique la altura de la persona en cm: ";
+
+btnEnv.addEventListener("click", () => {
+  let alturaPermitida: number = 129;
+  let alturaPersona: number = dato.value;
+  if (alturaPersona <= alturaPermitida) {
+    console.log("Hace falta medir al menos 130 cm para ingresar");
+    console.log(
+      "La persona mide",
+      `${alturaPersona}`,
+      "cm no puede subir al juego"
+    );
+    mensaje.innerHTML = "La persona mide " + alturaPersona + "cm";
+  } else {
+    console.log("La persona puede subir al juego");
+  }
+});
